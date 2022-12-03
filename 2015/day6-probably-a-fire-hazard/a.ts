b9cc6986a99f3e47f3d1b6dbc7ede7c3f0c6157b
rasvanB <not.obelix@gmail.com>
2022-12-04 00:30:15 +0200

    day6 part1

---
+++ 2015/day6-probably-a-fire-hazard/a.ts
@@ -0,0 +1,41 @@
+const input = Deno.readTextFileSync("input.txt").split("\r\n");
+
+enum actions {
+  "on" = 1,
+  "off" = 0,
+  "toggle" = 2,
+}
+
+const litLights: number[][] = new Array(1000)
+  .fill(null)
+  .map((_) => new Array(1000).fill(null).map((_) => 0));
+
+for (const text of input) {
+  const line = text.split(" ");
+
+  const [from, to] = [
+    line.slice(-3)[0].split(","),
+    line.slice(-3)[2].split(","),
+  ].map((v) => v.map(Number));
+
+  const action =
+    line.length === 4
+      ? actions.toggle
+      : line[1] === "on"
+      ? actions.on
+      : actions.off;
+
+  for (let i = from[0]; i <= to[0]; i++) {
+    for (let j = from[1]; j <= to[1]; j++) {
+      litLights[i][j] =
+        action === actions.toggle ? 1 - litLights[i][j] : action;
+    }
+  }
+}
+
+const total = litLights.reduce(
+  (prev, row) => (prev += row.reduce((prev, light) => (prev += light), 0)),
+  0
+);
+
+console.log(total);
