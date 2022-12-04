d64d0867e4e4bb7a5f9ee8fe6e2b0a4efd414e11
rasvanB <not.obelix@gmail.com>
2022-12-04 04:14:21 +0200

    finished both parts

---
+++ 2015/day7-some-assembly-required/a.js
@@ -0,0 +1,50 @@
+const input = Deno.readTextFileSync("input.txt").split("\r\n");
+
+const wires = new Map();
+
+const tryCast = (value) => {
+  return !isNaN(Number(value)) ? Number(value) : value;
+};
+
+const BITWISE_METHODS = {
+  AND: (a, b) => a & b,
+  OR: (a, b) => a | b,
+  NOT: (a) => ~a & 0xffff,
+  LSHIFT: (a, b) => a << b,
+  RSHIFT: (a, b) => a >> b,
+};
+
+const calculateWire = (input) => {
+  if (typeof input === "number") return input;
+  const value = wires.get(input);
+
+  if (!value) return;
+  if (typeof value === "number") return value;
+
+  let operand1, command, operand2;
+  if (value.split(" ").length === 3) {
+    [operand1, command, operand2] = value.split(" ");
+    operand1 = tryCast(operand1);
+    operand2 = tryCast(operand2);
+    wires.set(
+      input,
+      BITWISE_METHODS[command](calculateWire(operand1), calculateWire(operand2))
+    );
+  } else if (value.split(" ").length === 2) {
+    [command, operand1] = value.split(" ");
+    operand1 = tryCast(operand1);
+    wires.set(input, BITWISE_METHODS["NOT"](calculateWire(operand1)));
+  } else {
+    wires.set(input, calculateWire(value));
+  }
+
+  return wires.get(input);
+};
+
+for (const line of input) {
+  const [left, target] = line.split(" -> ");
+  wires.set(target, tryCast(left));
+}
+
+calculateWire("a");
+console.log(wires.get("a"));
